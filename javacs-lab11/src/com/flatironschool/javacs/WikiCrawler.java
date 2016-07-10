b4779c9fc2a5ea5a6910b0d9440046e8d3cd8158
Devang101 <devang397@gmail.com>
2016-07-10 17:05:17 -0500

    Done

--- javacs-lab11/src/com/flatironschool/javacs/WikiCrawler.java
+++ javacs-lab11/src/com/flatironschool/javacs/WikiCrawler.java
@@ -15,19 +15,19 @@ import redis.clients.jedis.Jedis;
 public class WikiCrawler {
 	// keeps track of where we started
 	private final String source;
-	
+
 	// the index where the results go
 	private JedisIndex index;
-	
+
 	// queue of URLs to be indexed
 	private Queue<String> queue = new LinkedList<String>();
-	
+
 	// fetcher used to get pages from Wikipedia
 	final static WikiFetcher wf = new WikiFetcher();
 
 	/**
 	 * Constructor.
-	 * 
+	 *
 	 * @param source
 	 * @param index
 	 */
@@ -39,43 +39,96 @@ public class WikiCrawler {
 
 	/**
 	 * Returns the number of URLs in the queue.
-	 * 
+	 *
 	 * @return
 	 */
 	public int queueSize() {
-		return queue.size();	
+		return queue.size();
 	}
 
 	/**
 	 * Gets a URL from the queue and indexes it.
-	 * @param b 
-	 * 
+	 * @param b
+	 *
 	 * @return Number of pages indexed.
 	 * @throws IOException
 	 */
-	public String crawl(boolean testing) throws IOException {
-        // FILL THIS IN!
-		return null;
+	public String crawl(boolean testing) throws IOException
+	{
+		if (queue.isEmpty()) {
+			return null;
+		}
+
+		//get first url from queue
+		String url = this.queue.remove();
+
+		if (testing == true)
+		{
+			//index page and add all internal urls to queue
+			crawlPage(url);
+		}
+
+		else
+		{
+			//if already indexed don't index again and return null
+			if(this.index.isIndexed(url))
+			{
+				return null;
+			}
+
+			//index page and add internal urls to queue
+			crawlPage(url);
+		}
+
+		return url;
 	}
-	
+
+	public void crawlPage(String url) throws IOException
+	{
+
+		//create WikiFetcher and read url
+		Elements paras = wf.readWikipedia(url);
+
+		//index url to Jedis
+		this.index.indexPage(url, paras);
+
+		//add internal links to queue
+		queueInternalLinks(paras);
+
+	}
+
 	/**
 	 * Parses paragraphs and adds internal links to the queue.
-	 * 
+	 *
 	 * @param paragraphs
 	 */
 	// NOTE: absence of access level modifier means package-level
-	void queueInternalLinks(Elements paragraphs) {
-        // FILL THIS IN!
+	void queueInternalLinks(Elements paragraphs)
+	{
+		//get urls on page and add to queue
+		for(Element paragraph : paragraphs)
+		{
+			Elements links = paragraph.select("a[href]");
+				for (Element link: links)
+				{
+					String newUrl = link.attr("href");
+					if (newUrl.startsWith("/wiki/"))
+					{
+						newUrl = "https://en.wikipedia.org" + newUrl;
+						queue.add(newUrl);
+					}
+				}
+		}
 	}
 
 	public static void main(String[] args) throws IOException {
-		
+
 		// make a WikiCrawler
 		Jedis jedis = JedisMaker.make();
-		JedisIndex index = new JedisIndex(jedis); 
+		JedisIndex index = new JedisIndex(jedis);
 		String source = "https://en.wikipedia.org/wiki/Java_(programming_language)";
 		WikiCrawler wc = new WikiCrawler(source, index);
-		
+
 		// for testing purposes, load up the queue
 		Elements paragraphs = wf.fetchWikipedia(source);
 		wc.queueInternalLinks(paragraphs);
@@ -85,10 +138,8 @@ public class WikiCrawler {
 		do {
 			res = wc.crawl(false);
 
-            // REMOVE THIS BREAK STATEMENT WHEN crawl() IS WORKING
-            break;
 		} while (res == null);
-		
+
 		Map<String, Integer> map = index.getCounts("the");
 		for (Entry<String, Integer> entry: map.entrySet()) {
 			System.out.println(entry);
